960469aeb3b44c7b8c20ace9e003f929dff74faa
turtleletortue <46635000+turtleletortue@users.noreply.github.com>
2019-12-03 19:14:47 -0500

    Add the backup script Madpinger made

---
+++ x18_partition_backup.sh
@@ -0,0 +1,52 @@
+#!/system/bin/sh
+# make nice later
+echo "Brought to you by the efforts of: Retro Game Handhelds Git Wiki and Discord"
+echo "Starting 1 of 24"
+dd if=/dev/block/platform/mtk-msdc.0/11230000.MSDC0/by-name/boot of=/storage/sdcard1/boot.img
+echo "Starting 2 of 24"
+dd if=/dev/block/platform/mtk-msdc.0/11230000.MSDC0/by-name/cache of=/storage/sdcard1/cache.img
+echo "Starting 3 of 24"
+dd if=/dev/block/platform/mtk-msdc.0/11230000.MSDC0/by-name/custom of=/storage/sdcard1/custom.img
+echo "Starting 4 of 24"
+dd if=/dev/block/platform/mtk-msdc.0/11230000.MSDC0/by-name/dkb of=/storage/sdcard1/dkb.img
+echo "Starting 5 of 24"
+dd if=/dev/block/platform/mtk-msdc.0/11230000.MSDC0/by-name/expdb of=/storage/sdcard1/expdb.img
+echo "Starting 6 of 24"
+dd if=/dev/block/platform/mtk-msdc.0/11230000.MSDC0/by-name/flashinfo of=/storage/sdcard1/flashinfo.img
+echo "Starting 7 of 24"
+dd if=/dev/block/platform/mtk-msdc.0/11230000.MSDC0/by-name/frp of=/storage/sdcard1/frp.img
+echo "Starting 8 of 24"
+dd if=/dev/block/platform/mtk-msdc.0/11230000.MSDC0/by-name/kb of=/storage/sdcard1/kb.img
+echo "Starting 9 of 24"
+dd if=/dev/block/platform/mtk-msdc.0/11230000.MSDC0/by-name/lk of=/storage/sdcard1/lk.img
+echo "Starting 10 of 24"
+dd if=/dev/block/platform/mtk-msdc.0/11230000.MSDC0/by-name/logo of=/storage/sdcard1/logo.img
+echo "Starting 11 of 24"
+dd if=/dev/block/platform/mtk-msdc.0/11230000.MSDC0/by-name/metadata of=/storage/sdcard1/metadata.img
+echo "Starting 12 of 24"
+dd if=/dev/block/platform/mtk-msdc.0/11230000.MSDC0/by-name/nvram of=/storage/sdcard1/nvram.img
+echo "Starting 13 of 24"
+dd if=/dev/block/platform/mtk-msdc.0/11230000.MSDC0/by-name/para of=/storage/sdcard1/para.img
+echo "Starting 14 of 24"
+dd if=/dev/block/platform/mtk-msdc.0/11230000.MSDC0/by-name/persist of=/storage/sdcard1/persist.img
+echo "Starting 15 of 24"
+dd if=/dev/block/platform/mtk-msdc.0/11230000.MSDC0/by-name/proinfo of=/storage/sdcard1/proinfo.img
+echo "Starting 16 of 24"
+dd if=/dev/block/platform/mtk-msdc.0/11230000.MSDC0/by-name/protect1 of=/storage/sdcard1/protect1.img
+echo "Starting 17 of 24"
+dd if=/dev/block/platform/mtk-msdc.0/11230000.MSDC0/by-name/protect2 of=/storage/sdcard1/protect2.img
+echo "Starting 18 of 24"
+dd if=/dev/block/platform/mtk-msdc.0/11230000.MSDC0/by-name/recovery of=/storage/sdcard1/recovery.img
+echo "Starting 19 of 24"
+dd if=/dev/block/platform/mtk-msdc.0/11230000.MSDC0/by-name/seccfg of=/storage/sdcard1/seccfg.img
+echo "Starting 20 of 24"
+dd if=/dev/block/platform/mtk-msdc.0/11230000.MSDC0/by-name/secro of=/storage/sdcard1/secro.img
+echo "Starting 21 of 24"
+dd if=/dev/block/platform/mtk-msdc.0/11230000.MSDC0/by-name/system of=/storage/sdcard1/system.img
+echo "Starting 22 of 24"
+dd if=/dev/block/platform/mtk-msdc.0/11230000.MSDC0/by-name/tee1 of=/storage/sdcard1/tee1.img
+echo "Starting 23 of 24"
+dd if=/dev/block/platform/mtk-msdc.0/11230000.MSDC0/by-name/tee2 of=/storage/sdcard1/tee2.img
+echo "Starting 24 of 24"
+dd if=/dev/block/platform/mtk-msdc.0/11230000.MSDC0/by-name/userdata of=/storage/sdcard1/userdata.img
+echo "Finished"
